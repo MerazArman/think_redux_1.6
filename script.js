91127020ffd2089aa912223a22f216095e3aab59
Meraz Arman <merazarman890@gmail.com>
2022-08-17 15:20:15 +0600

    dynamic vareble end action creator payloads setup

--- script.js
+++ script.js
@@ -8,16 +8,35 @@ const initialState = {
     value: 0,
 }
 
+//action identifier
+const increment = "increment";
+const decrement = "decrement";
+
+//action creators
+const incrementAction = (value)=>{
+   return {
+    type: increment,
+    payload: value,
+}
+}
+
+const decrementAction = (value)=>{ 
+    return {
+     type: decrement,
+     payload: value,
+ }
+ }
+ 
 function reducerCounter (currentState = initialState, action){
-    if (action.type === "increment") {
+    if (action.type === increment) {
         return{
             ...currentState,
-            value: currentState.value + 1
+            value: currentState.value + action.payload
         }
-    }else if (action.type === "decrement") {
+    }else if (action.type === decrement) {
         return{
             ...currentState,
-            value: currentState.value - 1
+            value: currentState.value - action.payload
         }
     }else{
         currentState
@@ -28,21 +47,18 @@ function reducerCounter (currentState = initialState, action){
 const storOfRedux = Redux.createStore(reducerCounter);
 
 function renderElement(){
-    const stateChange = storOfRedux.getState();
+    let stateChange = storOfRedux.getState();
+    console.log(stateChange);
     counterEl.innerText = stateChange.value;
 };
-
 
 storOfRedux.subscribe(renderElement)
 
 incrementEl.addEventListener("click", () => {
-    storOfRedux.dispatch({
-        type: "increment",
-    })
+    storOfRedux.dispatch(incrementAction(5))
 })
 
 decrementEl.addEventListener("click", () => {
-    storOfRedux.dispatch({
-        type: "decrement",
-    })
+    storOfRedux.dispatch(decrementAction(2))
 })
+
